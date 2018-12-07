ebd38f57a87642886cfa4ebe999fccdf4bc4594e
fardinkhan <fardinkhan@uvic.ca>
2018-12-07 02:07:36 -0800

    has 2 files inside

---
+++ basic file-stat calculator/compute_stats.py
@@ -0,0 +1,28 @@
+import fileinput
+
+def compute_stats (date1, date2):
+	high = 0
+	low = 10000
+	total_volume = 0
+	x = 0
+	for line in fileinput.input():
+		line = line.split()
+		if int(line[0]) >= int(date1) and int(line[0]) <= int(date2):
+			if float(line[2]) > high:
+				high = float(line[2])
+			if float(line[3]) < low:
+				low = float(line[3])
+			total_volume += int(line[5])
+			x += 1
+
+	total_volume = int(total_volume / x)
+
+	print("Highest: {}".format(high))
+	print("Lowest: {}".format(low))
+	print("total_volume : {}".format(total_volume))
+
+compute_stats("20130311", "20130313")
+
+
+
+
